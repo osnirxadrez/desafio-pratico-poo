30fb15d41ddf8111d4a988075ed3ef79d770ed91
Osnir <42158050+osnirxadrez@users.noreply.github.com>
2023-04-02 18:14:37 -0300

    Delete desing-pattern-practice directory

--- desing-pattern-practice/src/main/java/br/osnircompany/desingpatternpractice/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.osnircompany.desingpatternpractice.repository;
-
-import br.osnircompany.desingpatternpractice.model.Pessoa;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
-}
